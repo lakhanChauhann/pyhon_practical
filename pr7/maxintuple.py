49dbd32e0caff6935ca697f6eade0c66a0c0bdbc
lakhan1803 <101450934+lakhan1803@users.noreply.github.com>
2022-03-12 23:42:10 +0530

    Add files via upload

---
+++ pr7/maxintuple.py
@@ -0,0 +1,3 @@
+a=(10,20,100,30,40)
+print("tuple:",a)
+print(max(a),"is the largest number in tuple")
---
+++ pr7/repeatintup.py
@@ -0,0 +1,6 @@
+a=(10,30,30,30,40,20,50)
+for i in a:
+    if(a.count(i)>1):
+        print(i,"is the repeated element")
+        break
+            
